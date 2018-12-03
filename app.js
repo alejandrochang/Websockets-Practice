f8180e9939e58853ff4e3bc1221e50aa78447020
Alejandro <alejandrochang10@gmail.com>
2018-12-03 12:19:08 -0800

    establishing client connection on websockets

--- app.js
+++ app.js
@@ -7,4 +7,47 @@ window.onload = function() {
     var closeBtn = document.getElementById('close');
 
     //Rest of code
+
+    var socket = new WebSocket('ws://echo.websocket.org');
+
+    socket.onopen = function(event) {
+        socketStatus.innerHTML = 'Connected to: ' + event.currentTarget.url;
+        socketStatus.className = 'open';
+    };
+
+    socket.onerror = function(error) {
+        console.log('WebSocket Error: ' + error);
+    };
+
+    form.onsubmit = function(e) {
+        e.preventDefault();
+
+        var message = messageField.value;
+
+        socket.send(message);
+
+        messagesList.innerHTML += '<li class="sent"><span>Sent:</span>' + message + '</li>';
+
+        messageField.value = '';
+
+        return false;
+    }
+
+    socket.onmessage = function(event) {
+        var message = event.data;
+        messagesList.innerHTML += '<li class="received"><span>Received:</span>' + message + '</li>';
+    };
+
+    socket.onclose = function(event) {
+        socketStatus.innerHTML = 'Disconnected from WebSocket.';
+        socketStatus.className = 'closed';
+    };
+
+    closeBtn.onclick = function(e) {
+        e.preventDefault();
+
+        socket.close();
+
+        return false;
+    }
 };
